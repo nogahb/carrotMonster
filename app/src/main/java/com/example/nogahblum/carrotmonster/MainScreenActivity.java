8ca8ed201dadbcfabb19feb7d481d67bb0aba78c
nogahb <nogah.blum@mail.huji.ac.il>
2018-09-01 13:07:24 +0300

    try to add tool bar

--- app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
+++ app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
@@ -1,13 +1,45 @@
 package com.example.nogahblum.carrotmonster;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainScreenActivity extends AppCompatActivity {
 
+    TextView m_toolbar_title;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        getSupportActionBar().hide();
+
+        m_toolbar_title = (TextView)findViewById(R.id.toolbar_title);
+        m_toolbar_title.setText("Activity Title");
+
     }
+
 }
+
+
+
+//my code:
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.os.Bundle;
+////import android.widget.Toolbar;
+//
+//public class MainScreenActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main_screen);
+////        getSupportActionBar().hide();
+//
+//        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//    }
+//}
