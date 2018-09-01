4b0c0b5efbf0f4c879dce9b09700bb2d91c300c2
nogahb <nogah.blum@mail.huji.ac.il>
2018-09-01 16:42:02 +0300

    add menu toolbar TODO: (1) make it visible only when touching corner. (2) how to make it inharitent from base activity

--- app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
+++ app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
@@ -1,45 +1,49 @@
 package com.example.nogahblum.carrotmonster;
 
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainScreenActivity extends AppCompatActivity {
 
-    TextView m_toolbar_title;
-
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        getSupportActionBar().hide();
 
-        m_toolbar_title = (TextView)findViewById(R.id.toolbar_title);
-        m_toolbar_title.setText("Activity Title");
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.monster_info:
+                intent = new Intent(this, MonsterInfo.class);
+                this.startActivity(intent);
+                return true;
+            case R.id.user_info:
+                intent = new Intent(this, UserInfo.class);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
-
-
-
-//my code:
-//import android.support.v7.app.AppCompatActivity;
-//import android.support.v7.widget.Toolbar;
-//import android.os.Bundle;
-////import android.widget.Toolbar;
-//
-//public class MainScreenActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main_screen);
-////        getSupportActionBar().hide();
-//
-//        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//    }
-//}
