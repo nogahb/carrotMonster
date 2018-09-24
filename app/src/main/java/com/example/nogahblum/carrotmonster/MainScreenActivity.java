88f4095f86b3c04212148af259556436b1232b24
nogahb <nogah.blum@mail.huji.ac.il>
2018-09-24 19:22:51 +0300

    Reshef timer changes

--- app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
+++ app/src/main/java/com/example/nogahblum/carrotmonster/MainScreenActivity.java
@@ -16,10 +16,11 @@ import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
-import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+//import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
 
 import java.util.ArrayList;
 
@@ -53,6 +54,27 @@ public class MainScreenActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.this_toolbar);
         setSupportActionBar(toolbar);
+
+        if (Session.curmonster != null){
+            String satet  = Session.curmonster.check_on_monster();
+            if (satet.equals("hungry")){
+                Toast.makeText(MainScreenActivity.this,
+                        "Feed Me", Toast.LENGTH_LONG).show();
+            }
+            if (satet.equals(("ded"))){
+                Toast.makeText(MainScreenActivity.this,
+                        "your monster is dead and reborn ", Toast.LENGTH_LONG).show();
+                Monster reborn_m = new Monster(Session.curmonster.getGoodFood(),Session.curmonster.getGoodFood(),
+                        Session.curmonster.type);
+                Session.curmonster =reborn_m;
+            }
+            if(satet.equals("OK")){
+                Toast.makeText(MainScreenActivity.this,
+                        " Nice to see you again "+Session.USER_NAME, Toast.LENGTH_LONG).show();
+
+
+            }
+        }
 
     }
 
@@ -95,7 +117,7 @@ public class MainScreenActivity extends AppCompatActivity {
         View rootView = inflater.inflate(R.layout.activity_main_screen, container, false);
 
             ImageView fabContent = new ImageView(getActivity());
-            fabContent.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_pass));
+            fabContent.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
 
 
                 com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton darkButton =
@@ -110,187 +132,106 @@ public class MainScreenActivity extends AppCompatActivity {
         ArrayList<String> badFoodArr = Session.curmonster.getBadFood();
 
         TextView[] textToButtons = new TextView[goodFoodArr.size() + badFoodArr.size()];
+        String[] buttonsNames = new String[goodFoodArr.size() + badFoodArr.size()];
 
         for( int i = 0; i < goodFoodArr.size(); i++ )
         {
             TextView textView = new TextView(this.getContext());
             textView.setText(Session.curmonster.getGoodFood().get(i));
             textToButtons[i] = textView;
+
+            buttonsNames[i] = Session.curmonster.getGoodFood().get(i);
         }
 
-        for( int i = goodFoodArr.size(); i < badFoodArr.size(); i++ )
+        for( int i = 0; i < badFoodArr.size(); i++ )
         {
             TextView textView = new TextView(this.getContext());
-            textView.setText(Session.curmonster.getGoodFood().get(i));
-            textToButtons[i] = textView;
+            textView.setText(Session.curmonster.getBadFood().get(i));
+            textToButtons[goodFoodArr.size() + i] = textView;
+
+            buttonsNames[goodFoodArr.size() + i] = Session.curmonster.getBadFood().get(i);
+
         }
 
             SubActionButton.Builder rLSubBuilder = new SubActionButton.Builder(getActivity())
-                    .setTheme(SubActionButton.THEME_DARK);
-            ImageView rlIcon1 = new ImageView(getActivity());
-            ImageView rlIcon2 = new ImageView(getActivity());
-            ImageView rlIcon3 = new ImageView(getActivity());
-            ImageView rlIcon4 = new ImageView(getActivity());
-            ImageView rlIcon5 = new ImageView(getActivity());
+                    .setTheme(SubActionButton.ACCESSIBILITY_LIVE_REGION_NONE);
 
-            rlIcon1.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
-            rlIcon2.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
-            rlIcon3.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
-            rlIcon4.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
-            rlIcon5.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
+            // Set all SubActionButtons
+            FloatingActionMenu centerBottomMenu;
 
-            // Set 4 SubActionButtons
-            FloatingActionMenu centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-                    .setStartAngle(0)
-                    .setEndAngle(-180)
-                    .setAnimationHandler(new SlideInAnimationHandler())
-                    .addSubActionView(rLSubBuilder.setContentView(rlIcon1).build())
-                    .addSubActionView(rLSubBuilder.setContentView(rlIcon2).build())
-                    .addSubActionView(rLSubBuilder.setContentView(rlIcon3).build())
-                    .addSubActionView(rLSubBuilder.setContentView(rlIcon4).build())
-                    .addSubActionView(rLSubBuilder.setContentView(rlIcon5).build())
-                    .attachTo(darkButton)
-                    .build();
+            switch (textToButtons.length) {
+                case 1:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+                case 2:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+                case 3:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+                case 4:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+                case 5:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[4]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+                case 6:
+                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
+                            .setStartAngle(0)
+                            .setEndAngle(-180)
+                            .setAnimationHandler(new SlideInAnimationHandler())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[4]).build())
+                            .addSubActionView(rLSubBuilder.setContentView(textToButtons[5]).build())
+                            .attachTo(darkButton)
+                            .build();
+                    break;
+            }
 
             return rootView;
         }
     }
 }
-
-
-
-/**
- * A fragment for the FEED_ME! button
- * (A placeholder fragment containing a simple view).
- */
-//public static class CustomAnimationDemoFragment extends Fragment {
-//
-//    public CustomAnimationDemoFragment() {
-//    }
-//
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        View rootView = inflater.inflate(R.layout.activity_main_screen, container, false);
-//
-//        // Create a button to attach the menu:
-//        ImageView fabContent = new ImageView(getActivity());
-//        fabContent.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_pass));
-//
-//        com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton darkButton =
-//                new com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.Builder(getActivity())
-//                        .setTheme(com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.THEME_DARK)
-//                        .setContentView(fabContent)
-//                        .setPosition(com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton.POSITION_BOTTOM_CENTER)
-//                        .build();
-//
-//        // Create menu items:
-//        ArrayList<String> goodFoodArr = Session.curmonster.getGoodFood();
-//        ArrayList<String> badFoodArr = Session.curmonster.getBadFood();
-//
-//        TextView[] textToButtons = new TextView[goodFoodArr.size() + badFoodArr.size()];
-//
-//        for( int i = 0; i < goodFoodArr.size(); i++ )
-//        {
-//            TextView textView = new TextView(this.getContext());
-//            textView.setText(Session.curmonster.getGoodFood().get(i));
-//            textToButtons[i] = textView;
-//        }
-//
-//        for( int i = goodFoodArr.size(); i < goodFoodArr.size() + badFoodArr.size(); i++ )
-//        {
-//            TextView textView = new TextView(this.getContext());
-//            textView.setText(Session.curmonster.getGoodFood().get(i));
-//            textToButtons[i] = textView;
-//        }
-//
-//        SubActionButton.Builder rLSubBuilder = new SubActionButton.Builder(getActivity())
-//                .setTheme(SubActionButton.THEME_DARK);
-//
-//        // Set all SubActionButtons
-//        FloatingActionMenu centerBottomMenu;
-//
-//        switch (textToButtons.length) {
-//            case 0:
-////                    ImageView rlIcon1 = new ImageView(getActivity());
-////                    rlIcon1.setImageDrawable(getResources().getDrawable(R.drawable.ic_action_user));
-////                    centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-////                            .setStartAngle(0)
-////                            .setEndAngle(-180)
-////                            .setAnimationHandler(new SlideInAnimationHandler())
-////                            .addSubActionView(rLSubBuilder.setContentView(rlIcon1).build())
-////                            .attachTo(darkButton)
-////                            .build();
-//                break;
-//            case 1:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//            case 2:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//            case 3:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//            case 4:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//            case 5:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[4]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//            case 6:
-//                centerBottomMenu = new FloatingActionMenu.Builder(getActivity())
-//                        .setStartAngle(0)
-//                        .setEndAngle(-180)
-//                        .setAnimationHandler(new SlideInAnimationHandler())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[0]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[1]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[2]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[3]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[4]).build())
-//                        .addSubActionView(rLSubBuilder.setContentView(textToButtons[5]).build())
-//                        .attachTo(darkButton)
-//                        .build();
-//                break;
-//        }
-//        return rootView;
-//    }
